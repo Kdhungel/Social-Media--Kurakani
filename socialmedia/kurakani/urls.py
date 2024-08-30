b5b435769780ab2bb268694e6c9acf0259d80ce4
Kritish <kritishdhungel@gmail.com>
2024-08-30 20:26:51 +0545

    Created Templates and URL Configs

--- socialmedia/kurakani/urls.py
+++ socialmedia/kurakani/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.kurakani_list, name = 'kurakani_list'),
+    path('create/', views.kurakani_create, name = 'kurakani_create'),
+    path('<int:kurakani_id>/edit/', views.kurakani_edit, name = 'kurakani_edit'),
+    path('<int:kurakani_id>/delete/', views.kurakani_delete, name = 'kurakani_delete'),
 ]
